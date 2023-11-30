51ad86db3e63277a216cc3fc5476d9c32b255f84
Professormood <92402981+Professormood@users.noreply.github.com>
2023-12-01 02:59:37 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      VeryDarkBlue: "hsl(243, 87%, 12%)",
+      DesaturatedBlue: "hsl(238, 22%, 44%)",
+      BrightBlue: "hsl(224, 93%, 58%)",
+      ModerateCyan: "hsl(170, 45%, 43%)",
+      LightGrayishBlue: "hsl(240, 75%, 98%)",
+      LightGray: "hsl(0, 0%, 75%)",
+      bgColor:"hsl(250, 75%, 98%)"
+    },
+    fontFamily:{
+      Raleway:"Raleway, sans-serif",
+      OpenSans:"Open Sans, sans-serif"
+    },
+    extend: {},
+  },
+  plugins: [],
+};
